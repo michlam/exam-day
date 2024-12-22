d7096b8a43a2de430b41cd9c358baf1788dad373
UBC Student <lam.michael.81201@gmail.com>
2024-12-22 15:32:01 -0800

    Added variables and signal calls

--- src/Game Interface/game_interface.gd
+++ src/Game Interface/game_interface.gd
@@ -4,6 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Game Interface Scene entered")
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	
 	# Check if a dialog is already running
 	if Dialogic.current_timeline != null:
@@ -11,3 +12,8 @@ func _ready() -> void:
 	
 	Dialogic.start("Timeline A")
 	get_viewport().set_input_as_handled()
+
+func _on_dialogic_signal(argument: String):
+	if argument == "activate_something":
+		print("Something was activated!")
+		print(Dialogic.VAR.get('my_var_1'))
