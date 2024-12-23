6a3b46d31ce0457ef9dc45a0f388f1a24abc8f6b
UBC Student <lam.michael.81201@gmail.com>
2024-12-22 19:03:23 -0800

    Added more location images

--- src/Game Interface/game_interface.gd
+++ src/Game Interface/game_interface.gd
@@ -3,15 +3,19 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Game Interface Scene entered")
-	Dialogic.signal_event.connect(_on_dialogic_signal)
-	
+	$AnimationPlayer.play("fade_in")
+	initialize_dialogic()
+
+
+func initialize_dialogic():
 	# Check if a dialog is already running
 	if Dialogic.current_timeline != null:
 		return
 	
 	Dialogic.start("Timeline A")
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	get_viewport().set_input_as_handled()
+
 
 func _on_dialogic_signal(argument: String):
 	if argument == "activate_something":
