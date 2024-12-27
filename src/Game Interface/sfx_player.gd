06bc426ebfcfd542e7ad0804aadaaec11fd33b5b
UBC Student <lam.michael.81201@gmail.com>
2024-12-26 16:23:17 -0800

    Added tween fadeout for alarm clock

--- src/Game Interface/sfx_player.gd
+++ src/Game Interface/sfx_player.gd
@@ -9,9 +9,17 @@ const audio_dict = {
 
 
 func play_sfx(audio) -> void:
+	var tween = create_tween()
+	
+	volume_db = -80
 	stream = audio_dict[audio]
 	play()
+	tween.tween_property(self, "volume_db", -25, 1)
 
 
 func stop_sfx() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "volume_db", -80, 1)
+	
+	await tween.finished
 	stop()
