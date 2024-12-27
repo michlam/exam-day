404da4f06e43f98024851e162f700e1b6508d787
UBC Student <lam.michael.81201@gmail.com>
2024-12-26 21:37:53 -0800

    Continued sound effects

--- src/Game Interface/sfx_player.gd
+++ src/Game Interface/sfx_player.gd
@@ -5,21 +5,18 @@ extends AudioStreamPlayer
 ############
 const audio_dict = {	
 	"alarm clock" = preload("res://assets/Audio/SFX/Alarm Clock.wav"),
+	"bus" = preload("res://assets/Audio/SFX/Bus.wav"),
 	"cooking" = preload("res://assets/Audio/SFX/Cooking.wav"),
 	"cutlery" = preload("res://assets/Audio/SFX/Cutlery.wav"),
-	"footsteps 1" = preload("res://assets/Audio/SFX/Footsteps_1.wav"),
+	"footsteps 1" = preload("res://assets/Audio/SFX/Footsteps 1.wav"),
 }
 
 
 func play_sfx(audio) -> void:
 	var tween = create_tween()
 	var tween_length = 1
-	var new_volume = 8
 	
-	if audio == "alarm clock":
-		new_volume = -20
-	if audio == "footsteps 1":
-		tween_length = 0
+	var new_volume = set_volume(audio)
 	
 	volume_db = -80
 	
@@ -27,6 +24,15 @@ func play_sfx(audio) -> void:
 	play()
 	tween.tween_property(self, "volume_db", new_volume, tween_length)
 
+func set_volume(audio):
+	if audio == "alarm clock":
+		return -20
+	if audio == "footsteps 1":
+		return -10
+	if audio == "bus":
+		return -6
+		
+	return 8
 
 func stop_sfx() -> void:
 	var tween = create_tween()
