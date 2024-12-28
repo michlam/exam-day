55c2b63fd03566476961ee1f2a516340474e318f
UBC Student <lam.michael.81201@gmail.com>
2024-12-27 18:50:31 -0800

    Removed autoskip for testing

--- src/Game Interface/game_interface.gd
+++ src/Game Interface/game_interface.gd
@@ -85,8 +85,3 @@ func _on_quit_button_pressed() -> void:
 	main_node.add_child(main_menu_instance)
 	
 	queue_free()
-
-
-func _input(event): # FOR TESTING ONLY!
-	if event.is_action_pressed("skip"):
-		Dialogic.Inputs.auto_skip.enabled = !Dialogic.Inputs.auto_skip.enabled
